64edf14c3b91023aa1688f207ec8767780b54590
wayandita <wayanbingo46@gmail.com>
2023-06-15 10:35:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasikita/Login.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasikita
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Login : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       setContentView(R.layout.login)
+}
+
+}
---
+++ app/src/main/java/com/example/aplikasikita/Produksemua.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasikita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Produksemua : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.produksemua)
+    }
+}
