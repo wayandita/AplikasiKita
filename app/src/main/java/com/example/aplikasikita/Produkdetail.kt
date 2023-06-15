7ffe35f5bb7caa0f9ebb31a6a586a7164f009eb8
wayandita <wayanbingo46@gmail.com>
2023-06-16 01:35:46 +0700

    menampilkan list data kategori dan menampilkan data produk

--- app/src/main/java/com/example/aplikasikita/Produkdetail.kt
+++ app/src/main/java/com/example/aplikasikita/Produkdetail.kt
@@ -2,8 +2,9 @@ package com.example.aplikasikita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class Produkdetail : AppCompatActivity() {
+class Produkdetail : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.produkdetail)
--- app/src/main/java/com/example/aplikasikita/Produksemua.kt
+++ app/src/main/java/com/example/aplikasikita/Produksemua.kt
@@ -2,10 +2,14 @@ package com.example.aplikasikita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class Produksemua : AppCompatActivity() {
+class Produksemua : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.produksemua)
+        val namas:List<String> = listOf("alesha voal 140","pasmina cerut","bergo zara","zayana voal");
+        val fotos:List<Int> = listOf(R.drawable.alesha,R.drawable.pasmina,R.drawable.bergo,R.drawable.zayana);
+
     }
 }
