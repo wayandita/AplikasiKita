ae68d76b84b67daa1b9f8b4444e694e83bfd9a8e
Alvin233Rama <ramadhan20311027@gmail.com>
2023-06-16 10:24:17 +0700

    Tambahan tampil data produk

--- app/src/main/java/com/example/aplikasikita/Kategori.kt
+++ app/src/main/java/com/example/aplikasikita/Kategori.kt
@@ -2,11 +2,27 @@ package com.example.aplikasikita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.ComponentActivity
 
 class Kategori : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.kategori)
+        setContentView(R.layout.kategori);
+
+//      interface list_kategori
+        val lv_kategori:ListView = findViewById(R.id.lv_kategori);
+
+//      membuat data kategori
+        val nama_kategoris:List<String> = listOf("Hijab","Pasmina","Mukena","Gamis","Aksesoris");
+
+//        membuat adapter yang berisi perulangan data
+        val perulangan_data:ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,nama_kategoris);
+
+//        pasang perulangan_data di interface lv_kategori
+        lv_kategori.adapter = perulangan_data;
+
+
     }
 }
